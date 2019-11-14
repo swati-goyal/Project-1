24067b944d1cac8a7fb3fe910b2d07fa6ccbe15a
swati_goyal@outlook.com <be9entia>
2019-11-14 22:41:19 +0530

    Initial search algorithm implemented

--- application.py
+++ application.py
@@ -2,12 +2,14 @@
 
 import os
 
-from flask import Flask, session, render_template, request, redirect, url_for
+from flask import Flask, session, render_template, request, redirect, url_for, flash
 from flask_session import Session
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 
+
 app = Flask(__name__)
+app.secret_key = '301289'
 
 # Check for environment variable
 if not os.getenv("DATABASE_URL"):
@@ -30,9 +32,33 @@ def index():
 def register():
     return render_template("register.html")
 
-@app.route("/login")
-def login():
-    return render_template("login.html")
+@app.route("/login", methods=['GET', 'POST'])
+def login_page():
+    if request.method == 'GET':
+        return render_template("login.html")
+    
+    if request.method == 'POST':
+        uname = request.form.get("username")
+        pswd = request.form.get("password")
+        validate_username = "Select * from users where username=:uname"
+        validate_user = "Select * from users where username=:uname and password=:pswd"
+
+        if (uname or pswd) is "":
+            return render_template("login.html", message="Username/Password can not be empty, both fields are required to login.")
+    
+        if db.execute(validate_username, {"uname":uname}).rowcount == 0:
+            return render_template("login.html", message="Invalid username. Please provide correct credentials to login.")
+    
+        if db.execute(validate_user, {"uname":uname, "pswd":pswd}).rowcount == 0:
+            return render_template("login.html", message="Invalid password. Please provide correct credentials to login.")
+        
+        session['username'] = uname
+        return redirect(url_for('books'))
+
+@app.route("/logout")
+def logout():
+    session.pop('username', None)
+    return render_template('index.html')
 
 @app.route("/user-registration", methods=["GET","POST"])
 def registration():
@@ -59,40 +85,39 @@ def registration():
         db.commit()
         return render_template("login.html", message="Successfully registered. Login to read or submit book reviews.")
     else:
+        flash("Username already exists!")
         return render_template("error.html", message="Username already exists. Please try a different username.") 
 
-@app.route("/user", methods=["POST"])
-def welcome():
-    uname = request.form.get("username")
-    pswd = request.form.get("password")
+@app.route('/search', methods=['GET'])
+def search():
+    query = request.args.get('q')
+    if not query:
+        flash("You did not search for anything")
+        return redirect(url_for('books'))
+    elif query:
+        q = '%'+ query +'%'
+        dbQuery = db.execute('select * from book where title ilike :query or isbn ilike :query or author ilike :query order by id limit 10', {"query": q})
+        res = dbQuery.fetchall()
+    return render_template('search.html', books=res)
 
-    validate_username = "Select * from users where username=:uname"
-    validate_user = "Select * from users where username=:uname and password=:pswd"
-
-    if (uname or pswd) is "":
-        return render_template("login.html", message="Username/Password can not be empty, both fields are required to login.")
-
-    if db.execute(validate_username, {"uname":uname}).rowcount == 0:
-        return render_template("login.html", message="Invalid username. Please provide correct credentials to login.")
-
-    if db.execute(validate_user, {"uname":uname, "pswd":pswd}).rowcount == 0:
-        return render_template("login.html", message="Invalid password. Please provide correct credentials to login.")
-    
-    return redirect(url_for('books'))
-    
 @app.route("/books")
 def books():
-    books = db.execute("Select * from book").fetchall()
-    return render_template("books.html", books=books, message="Welcome")
+    if 'username' in session:
+        uname = session['username']
+        books = db.execute("Select * from book").fetchall()
+        return render_template("books.html", books=books, message="Welcome "+ uname)
+
+    return render_template('error.html', message="Please login to access books.")
 
 @app.route("/book/<int:book_id>")
 def book(book_id):
-    book = db.execute("select * from book where id=:id",{"id":book_id}).fetchone()
-
-    if book is None:
-        return render_template("error.html", message="Book ID doesn't exist.")
-    
-    return render_template("book.html", book=book)
+    if 'username' in session:
+        uname = session['username']
+        book = db.execute("select * from book where id=:id",{"id":book_id}).fetchone()
+        if book is None:
+            return render_template("error.html", message="Book ID doesn't exist.")
+        return render_template("book.html", book=book)
+    return render_template('error.html', message="Please login to access books.")
 
 # @app.route("/user/<int:user_id>")
 # def profile(user_id):
---
+++ user-login.py
@@ -0,0 +1,31 @@
+from flask_login import UserMixin
+from werkzeug.security import generate_password_hash, check_password_hash
+
+
+class DbUser(object):
+    """Wraps User object for Flask-Login"""
+    def __init__(self, user):
+        self._user = user
+
+    def get_id(self):
+        return unicode(self._user.id)
+
+    def is_active(self):
+        return self._user.enabled
+
+    def is_anonymous(self):
+        return False
+
+    def is_authenticated(self):
+        return True
+    
+    def set_password(self, password):
+        """Create hashed password."""
+        self.password = generate_password_hash(password, method='sha256')
+
+    def check_password(self, password):
+        """Check hashed password."""
+        return check_password_hash(self.password, password)
+
+    def __repr__(self):
+        return '<User {}>'.format(self.username)
